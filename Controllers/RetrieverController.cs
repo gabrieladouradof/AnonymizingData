66e77e27e24c5d2b5fd7fb67aa60e8b045710217
gabrieladourado <gdourado.066@gmail.com>
2024-07-02 10:35:28 -0300

    add migrations

--- Controllers/RetrieverController.cs
+++ Controllers/RetrieverController.cs
@@ -5,17 +5,17 @@ using Newtonsoft.Json.Linq;
 using ProtectedDba.Models;
   
   namespace ProtectedDba.Controllers {
-  private string HashCPF (string originalCPF)
-      {
-         using (SHA256 sha256Hash = SHA256.Create())
-         {
-            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(originalCPF));
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < bytes.Length; i++) 
-            {
-               builder.Append(bytes[i].ToString("x2"));
-            }
+   private string HashCPF (string originalCPF)
+       {
+          using (SHA256 sha256Hash = SHA256.Create())
+          {
+             byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(originalCPF));
+             StringBuilder builder = new StringBuilder();
+             for (int i = 0; i < bytes.Length; i++) 
+             {
+                builder.Append(bytes[i].ToString("x2"));
+             }
+
             return builder.ToString();
          }
-      }
-  }
+       } }
---
+++ Migrations/20240702133322_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProtectedDba.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "UserAnonymized",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Cpf = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Gender = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    HashKey = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAnonymized", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Cpf = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    gender = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAnonymized");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
